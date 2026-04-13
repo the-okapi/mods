014708f5d52a6c29d149c26d013893a49e763e89
Theo Kapi <theo_kapi@outlook.com>
2026-04-13 07:07:07 -0700

    One more thing

---
+++ debatecraft/src/main/java/com/unlimitedstuffltd/mixin/PlayerMixin.java
@@ -0,0 +1,8 @@
+package com.unlimitedstuffltd.mixin;
+
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Player.class)
+abstract class PlayerMixin {
+}
